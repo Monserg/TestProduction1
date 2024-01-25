e0a3984ba49873549457deb05e1b1cff5b19c631
Serhii Monastyrskyi <smonastyrskyi@engenious.io>
2024-01-25 11:44:18 +0200

    Initial Commit

---
+++ TestProduction1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProduction1
+//
+//  Created by Serhii Monastyrskyi on 25.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
